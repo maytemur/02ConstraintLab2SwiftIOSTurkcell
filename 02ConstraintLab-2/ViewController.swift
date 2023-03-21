3d30ad2ca55bb662374b0573e6a2da5a66728b47
Mustafa Aytemür <maytemur@gmail.com>
2023-03-21 16:50:35 +0300

    Initial Commit

---
+++ 02ConstraintLab-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  02ConstraintLab-2
+//
+//  Created by maytemur on 27.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
